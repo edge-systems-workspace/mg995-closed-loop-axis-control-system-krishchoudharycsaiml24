6e6a045e1097f4296564fbae2515425d816c3522
krish choudhary <krish.choudhary_cs.aiml24@gla.ac.in>
2026-02-20 15:03:22 +0530

    Initialize Servo object and PWM pin
    Added Servo object and defined PWM pin for control.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+
+// Create Servo object
+Servo axisServo;
+
+// Define PWM pin (Use pin 9)
+const int SERVO_PIN = 9;
+
+// Variable to store servo angle
+int targetAngle = 0;
+
 void setup() {
 // write your initialization code here
 }
